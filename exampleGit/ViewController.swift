c3f2f42a2359b6a7154d0f9ec64b31965e8ed6eb
madendinara <madendinara@gmail.com>
2020-01-16 13:49:27 +0600

    Initial Commit

---
+++ exampleGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exampleGit
+//
+//  Created by Alfa on 1/16/20.
+//  Copyright © 2020 Alfa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
